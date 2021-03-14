8b948495ae4db4d054a917e7ce1a8c78ce1bd2a7
mwcodebase <mwaldron.personal@gmail.com>
2021-03-13 21:06:37 -0500

    updated for version checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+cd /github/workspace
+
+branch=$(git branch --show-current)
+git diff --name-only $branch master >> changed.txt
+
+if [ ! grep -Fxq "APP_VERSION" changed.txt ]
+then
+  echo "APP_VERSION file not updated."
+  exit 1
+fi
+
+if [ ! grep -Fxq "CHANGELOG.md" changed.txt ]
+then
+  echo "CHANGELOG.md file not updated."
+  exit 1
+fi
