802d6e4692aef5e1db99171c871e86ff104019ab
mwcodebase <mwaldron.personal@gmail.com>
2021-03-14 11:01:41 -0400

    ls testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/sh -l
 
-cd /github/workspace
+token=$1
+echo $GITHUB_REPOSITORY
+pwd
+ls -lah
 
-branch=$(git branch --show-current)
-git diff --name-only $branch master >> changed.txt
+# branch=$(git branch --show-current)
+# git diff --name-only $branch master >> changed.txt
 
-if ! grep -Fxq "APP_VERSION" changed.txt
-then
-  echo "APP_VERSION file not updated."
-  exit 1
-fi
+# if ! grep -Fxq "APP_VERSION" changed.txt
+# then
+#   echo "APP_VERSION file not updated."
+#   exit 1
+# fi
 
-if ! grep -Fxq "CHANGELOG.md" changed.txt
-then
-  echo "CHANGELOG.md file not updated."
-  exit 1
-fi
+# if ! grep -Fxq "CHANGELOG.md" changed.txt
+# then
+#   echo "CHANGELOG.md file not updated."
+#   exit 1
+# fi
