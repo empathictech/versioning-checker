3c7431afc169ff7fa434d2587d6b90045ba4e8d8
mwcodebase <mwaldron.personal@gmail.com>
2021-03-13 21:29:43 -0500

    removed brackets

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ cd /github/workspace
 branch=$(git branch --show-current)
 git diff --name-only $branch master >> changed.txt
 
-if [ ! grep -Fxq "APP_VERSION" changed.txt ]
+if ! grep -Fxq "APP_VERSION" changed.txt
 then
   echo "APP_VERSION file not updated."
   exit 1
 fi
 
-if [ ! grep -Fxq "CHANGELOG.md" changed.txt ]
+if ! grep -Fxq "CHANGELOG.md" changed.txt
 then
   echo "CHANGELOG.md file not updated."
   exit 1
