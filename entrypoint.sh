a8a758bbef246d2b0aedf12733c0fbe8b871baa2
mwcodebase <mwaldron.personal@gmail.com>
2021-03-14 13:35:35 -0400

    finaled branch grab

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,22 @@
 #!/bin/sh
 
-echo ${GITHUB_REF}
-echo ${GITHUB_HEAD_REF}
-echo ${GITHUB_BASE_REF}
-echo ${GITHUB_ACTION_REF}
+branch=${GITHUB_HEAD_REF}
+clone_link="https://github.com/$GITHUB_REPOSITORY.git"
 
-# clone_link="https://github.com/$GITHUB_REPOSITORY.git"
+git clone $clone_link repo
+cd repo
+git checkout $branch
 
-# git clone $clone_link repo
-# cd repo
-# git checkout $branch
+git diff --name-only $branch master >> changed.txt
 
-# git diff --name-only $branch master >> changed.txt
+if ! grep -Fxq "APP_VERSION" changed.txt
+then
+  echo "APP_VERSION file not updated."
+  exit 1
+fi
 
-# if ! grep -Fxq "APP_VERSION" changed.txt
-# then
-#   echo "APP_VERSION file not updated."
-#   exit 1
-# fi
-
-# if ! grep -Fxq "CHANGELOG.md" changed.txt
-# then
-#   echo "CHANGELOG.md file not updated."
-#   exit 1
-# fi
+if ! grep -Fxq "CHANGELOG.md" changed.txt
+then
+  echo "CHANGELOG.md file not updated."
+  exit 1
+fi
