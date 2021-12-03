9c253d4372c63d03bfb0001a5b136168154a3e02
mwcodebase <mwaldron.personal@gmail.com>
2021-12-03 13:00:45 -0500

    updated changed files logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,19 +11,24 @@ CLONE_LINK="https://github.com/$GITHUB_REPOSITORY.git"
 
 # Check if input pr is empty
 if [[ -n $INPUT_PR_SOURCE_REPO ]]; then
+  # Replace CLONE_LINK
   CLONE_LINK="https://github.com/$INPUT_PR_SOURCE_REPO.git"
 fi
 
-# Clone repo (sent from action.yml)
+# Clone repo
 echo "Cloning $CLONE_LINK"
-git clone https://github.com/$CLONE_LINK repo
+git clone $CLONE_LINK repo
 
 # Checkout the current branch
 cd repo
 git checkout $CURR_BRANCH
 
 # Collect changed files
-git diff --name-only $GITHUB_REPOSITORY/$BASE_BRANCH >> changed.txt
+if [[ -n $INPUT_PR_SOURCE_REPO ]]; then
+  git diff --name-only $INPUT_PR_SOURCE_REPO/$CURR_BRANCH $GITHUB_REPOSITORY/$BASE_BRANCH >> changed.txt
+else
+  git diff --name-only $CURR_BRANCH $BASE_BRANCH >> changed.txt
+fi
 
 # Collect tracked files
 IFS="," read -a tracked_files <<< $INPUT_TRACKED_FILES
