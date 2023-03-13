7b120a53ab1b14c4fa34891b6b99e53d14c3960c
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-13 11:48:38 -0400

    tracked fields rough draft

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 result=0
 
-CURR_BRANCH=${GITHUB_HEAD_REF}
-echo "Current branch: $CURR_BRANCH"
-
-BASE_BRANCH=${GITHUB_BASE_REF}
-echo "Comparing to: $BASE_BRANCH"
-
 CLONE_LINK="https://github.com/$GITHUB_REPOSITORY.git"
 
 # Check if input pr is empty
@@ -17,11 +11,19 @@ fi
 
 # Clone repo
 echo "Cloning $CLONE_LINK"
-git clone $CLONE_LINK repo
+echo ""
+git clone --quiet $CLONE_LINK repo
 
 # Checkout the current branch
+CURR_BRANCH=${GITHUB_HEAD_REF}
+echo "Current branch: $CURR_BRANCH"
+
+BASE_BRANCH=${GITHUB_BASE_REF}
+echo "Comparing to:   $BASE_BRANCH"
+echo ""
+
 cd repo
-git checkout $CURR_BRANCH
+git checkout --quiet $CURR_BRANCH
 
 # Collect changed files
 if [[ -n $INPUT_FORK_REPO_NAME ]]; then
@@ -36,12 +38,62 @@ fi
 IFS="," read -a tracked_files <<< $INPUT_TRACKED_FILES
 
 # Print any unchanged tracked files
-echo ""; echo "Checking for changes in ${tracked_files[@]}..."; echo ""
-for f in ${tracked_files[@]}; do
-  if ! grep -Fxq "$f" changed.txt; then
-    echo "$f has not been updated"
+echo "Checking for changes in..."
+echo "${tracked_files[@]}"
+echo ""
+
+for (( curr_file in ${tracked_files[@]} )); do
+  if (( ! grep -Fxq "$curr_file" changed.txt )); then
+    echo "$curr_file has not been updated"
+    echo ""
     result=1
   fi
 done
+
+# Collect tracked fields
+IFS="," read -a tracked_fields <<< $INPUT_TRACKED_FIELDS
+
+# Print version number findings
+if (( ${#tracked_fields[@]} % 2 == 0 )); then
+  # Define arrays
+  files=()
+  fields=()
+  matches=()
+
+  # Allows us to treat matches like a set
+  function add_unique_value() {
+    local value=$1
+
+    if [[  ! "${my_array[@]}" =~ "${value}" ]]; then
+      matches+=("$value")
+    fi
+  }
+
+  # Split the pairs
+  for (( i=0; i<${#tracked_fields[@]}; i+=2 )); do
+    files+=(${tracked_fields[$i]})
+    fields+=(${tracked_fields[$i+1]})
+  done
+
+  # Run the searches
+  for (( i=0; i<${#files[@]}; i+=1 )); do
+    match=$(grep -oE "${fields[$i]}" "${files[$i]}" | head -n1)
+    echo "${files[$i]} returned $match"
+    echo ""
+    add_unique_value "$match"
+  done
+
+  if [ "${#matches[@]}" -ne 1 ]; then
+    echo "Multiple (or no) version numbers found"
+    echo "Found: ${matches[@]}"
+    echo ""
+    result=1
+  fi
+
+else
+  echo "ERROR in tracked_fields: array of odd length found. Please provide a list of pairs."
+  echo ""
+  result=1
+fi
 
 exit $result
